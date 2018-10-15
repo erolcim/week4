1942322a60d7c25e6dee186a40640dfe0dc5c253
erolcim <44162098+erolcim@users.noreply.github.com>
2018-10-15 12:22:03 +0100

    Update diff.py

--- diff.py
+++ diff.py
@@ -1,11 +1,11 @@
 # week4
-class week for introduction to software
-Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
-Type "copyright", "credits" or "license()" for more information.
+#class week 4 introduction to software classwork
+#Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+#Type "copyright", "credits" or "license()" for more information.
 >>> x=20
 >>> a=15
 >>> x-a
-5
+5#I tried this method and it gave me the correct answer
 # this is the difference between x and a
 >>> 
 x=10
@@ -16,7 +16,7 @@ x/a= 5.0
 >>> x=20
 >>> a=15
 >>> print("x-a")
-x-a
+x-a #
 >>> print(x-a)
 5
 >>> 
